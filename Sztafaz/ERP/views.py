6197f62796b8cbe0cc6702034aae6d7c67d980cf
rakieciarz <piotrfiuk.pf@gmail.com>
2023-06-29 00:07:20 +0200

    Added first function add_purchases, login and new views

--- Sztafaz/ERP/views.py
+++ Sztafaz/ERP/views.py
@@ -3,38 +3,53 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm
 
+
 # Create your views here.
 
 
 def home(request):
-
+    # check if user is logged
     if request.user.is_authenticated:
+        # return home page if method is GET
         if request.method == 'GET':
             print(request.user)
             return render(request, 'ERP/home.html')
+        # if method is POST
         else:
-            query = request.POST['q']
-            if query == '1':
-                return render(request, 'ERP/add_purchase.html')
+            # Check if requested function exists
+            query = search(request.POST['q'])
+            # If so, return the given function
+            if query is not None:
+                return render(request, query)
             else:
-                return render(request, 'ERP/home.html')
+                return render(request, 'ERP/home.html', {'error': 'No function you called'})
     else:
         return redirect('loginuser')
 
 
-def search(request):
-    query = request.GET.get('q')
-
+def search(query):
+    # Add another function No. and path to HTML file
+    mapping = {'1': 'ERP/add_purchase.html',
+               }
+    # Check if function exists
+    try:
+        return mapping[query]
+    except KeyError:
+        return None
 
 
 def loginuser(request):
+    # return authentication form to logging if method GET
     if request.method == 'GET':
         return render(request, 'ERP/login.html', {'form': AuthenticationForm})
+    # check if given username and password is correct
     else:
         user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        # if so, login and return home page
         if user is not None:
             login(request, user)
             return redirect('home')
+        # raise error that username or password didnt match
         else:
             return render(request, 'ERP/login.html', {'form': AuthenticationForm, 'error': 'Invalid password or username'})
 
